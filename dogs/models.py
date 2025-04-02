fd2d48f3f0b1e4015accf94d76742362baab0ced
romz987pc <eesistance@gmail.com>
2025-04-02 23:53:56 +0300

    feat:  1.add NULLABLE data structure;  2.create model Dog;  3.install pillow;  4.migrations

--- dogs/models.py
+++ dogs/models.py
@@ -11,3 +11,26 @@ class Breed(models.Model):
     class Meta:
         verbose_name = 'breed'
         verbose_name_plural = 'breeds'
+
+
+# My metamodel 
+class Dog(models.Model):
+    name = models.CharField(max_length=250, verbose_name='dog name')
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name='breed')
+    photo = models.ImageField(upload_to='dogs/', **NULLABLE, verbose_name='image')
+    birth_date = models.DateField(**NULLABLE, verbose_name='birth_date')
+
+    def __str__(self):
+        return f'{self.name} ({self.breed})'
+
+    class Meta: 
+        verbose_name = 'dog'
+        verbose_name_plural = 'dogs'
+
+        abstract = True 
+        app_lable = 'dogs'
+        ordering = [-1]
+        proxy = True 
+        permissions = []
+        db_table = 'doggies'
+        get_latest_by = 'birth_date'
