1983235295b3722733a38eaed875a8633ca574bc
romz987pc <eesistance@gmail.com>
2025-04-03 00:08:15 +0300

    feat:  1.register models breed and dog in admin.py;

--- dogs/admin.py
+++ dogs/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from dogs.models import Breed, Dog
 
 # Register your models here.
+# Breed
+@admin.register(Breed)
+class BreedAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name')
+    ordering = ('pk', )
+
+
+# Dog
+@admin.register(Dog)
+class DogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'breed')
+    list_filter = ('breed', )
+    ordering = ('name', )
