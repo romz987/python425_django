65b17260bf37fa53b134205c24339cde430e2d43
romz987pc <eesistance@gmail.com>
2025-04-05 00:47:25 +0300

    docs: add comments for admin.py

--- dogs/admin.py
+++ dogs/admin.py
@@ -12,6 +12,10 @@ class BreedAdmin(admin.ModelAdmin):
 # Dog
 @admin.register(Dog)
 class DogAdmin(admin.ModelAdmin):
+    # поля, которые будут отображатсья в админке
     list_display = ('pk', 'name', 'breed')
+    # поля, по которым можно будет сортировать объекты модели в админке
     list_filter = ('breed', )
+    # как будут сортироваться объекты модели в административной панели
+    # по полю 'name' в данном случае
     ordering = ('name', )
