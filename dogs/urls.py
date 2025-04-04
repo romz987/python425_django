9834db3b56e4ef4333cd894bea628735d3fd00d5
romz987pc <eesistance@gmail.com>
2025-04-05 01:44:57 +0300

    fix: name index_view to index

--- dogs/urls.py
+++ dogs/urls.py
@@ -14,7 +14,7 @@ app_name = DogsConfig.name
 
 urlpatterns = [
     # breads
-    path('', index_view, name='index_view'),
+    path('', index_view, name='index'),
     path('breeds/', breeds_list_view, name='breeds'),
     path('breeds/<int:pk>/dogs', breed_dogs_list_view, name='breed_dogs'),
 
