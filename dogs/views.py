1792bf9122d812036c067eee1ae63d2709beca0b
romz987pc <eesistance@gmail.com>
2025-04-10 14:05:36 +0300

    fix: breed_dogs_list_view

--- dogs/views.py
+++ dogs/views.py
@@ -28,7 +28,7 @@ def breed_dogs_list_view(request, pk: int):
     breed_object = Breed.objects.get(pk=pk)
     context = {
         'objects_list': Dog.objects.filter(breed_id=pk),
-        'title': f'Собаки породы - {Breed.objects.name}',
+        'title': f'Собаки породы - {breed_object}',
         'breed_pk': breed_object.pk
     }
     return render(request, 'dogs/dogs.html', context=context)
