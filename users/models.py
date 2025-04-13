6bf67d4aa01dafe06f911120a8d9b4975da49774
romz987pc <eesistance@gmail.com>
2025-04-13 23:51:25 +0300

    fix: 1.add new fields to User model;  2.migrate;

--- users/models.py
+++ users/models.py
@@ -9,8 +9,11 @@ class User(AbstractUser):
 
     username = None 
     email = models.EmailField(unique=True, verbose_name='email')
+    first_name = models.CharField(max_length=150, verbose_name='Имя', default='Anonymous')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия', default='Anonymous')
     phone = models.CharField(max_length=35, unique=True, verbose_name='Номер телефона', **NULLABLE)
     telegram = models.CharField(max_length=150, unique=True, verbose_name='Телеграм', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='Аватар', **NULLABLE)
     is_active = models.BooleanField(default=True, verbose_name='Активность')
 
     USERNAME_FIELD = 'email'
