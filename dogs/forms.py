bf79f6247ddd6d4b8e17d31541f76d60d05efaed
romz987pc <eesistance@gmail.com>
2025-04-14 23:59:19 +0300

    feat: 1.create StyleFromMixin class;  2.add StyleFormMixin class to dogs/forms.py;  3.add StyleFormMixin class to users/forms.py;

--- dogs/forms.py
+++ dogs/forms.py
@@ -2,8 +2,10 @@ from django import forms
 from dogs.models import Dog 
 from django.forms import DateInput
 
+from users.forms import StyleFormMixin
 
-class DogForm(forms.ModelForm):
+
+class DogForm(StyleFormMixin, forms.ModelForm):
     
     class Meta:
         model = Dog 
--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,16 @@ from django import forms
 from users.models import User 
 
 
-class UserRegisterForm(forms.ModelForm):
+
+class StyleFormMixin:
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class UserRegisterForm(StyleFormMixin, forms.ModelForm):
 
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Повторите пароль', widget=forms.PasswordInput)
@@ -21,7 +30,7 @@ class UserRegisterForm(forms.ModelForm):
         return cleaned_data['password2']
 
 
-class UserLoginForm(forms.Form):
+class UserLoginForm(StyleFormMixin, forms.Form):
 
     email = forms.EmailField()
     password = forms.CharField(label='Пароль', widget=forms.PasswordInput)
