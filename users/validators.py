21b6f6fbf7e7b7dd435a55203bd30cd86d17b0de
romz987pc <eesistance@gmail.com>
2025-04-22 23:45:47 +0300

    feat: add multilang to validator

--- users/validators.py
+++ users/validators.py
@@ -1,25 +1,29 @@
-import re 
+import re
+from django.conf import settings 
 from django.core.exceptions import ValidationError 
 
 
 # Валидация пароля
 def validate_password(field):
     pattern = re.compile(r'^[A-Za-z0-9]+$')
-    if not bool(re.match(pattern, field)):
-        print(
-            'Пароль должен содержать только ' 
-            'латинские буквы и цифры'
-        )
-        raise ValidationError(
-            'Пароль должен содержать только ' 
-            'латинские буквы и цифры'
-        )
-    if not 8 <= len(field) <= 16:
-        print(
-            'Пароль должен содержать '
-            'от 8 до 16 символов'
-        )
-        raise ValidationError(
-            'Пароль должен содержать '
-            'от 8 до 16 символов'
-        )
+    language = setttings.LANGUAGE_CODE
+    error_messages = [
+        {
+            'ru-ru': 'Пароль должен содержать только латинские буквы и цифры',
+            'en-us': 'Password must contain A-Z a-z letters and 0-9 numbers'
+        },
+        {
+            'ru-ru': 'Пароль должен содержать от 8 до 16 символов',
+            'en-en': 'Password length must be between 8 and 16 characters'
+        }
+    ]
+    try:
+        if not bool(re.match(pattern, field)):
+            print(error_messages[0][language])
+            raise ValidationError(error_messages[0][language])
+        if not 8 <= len(field) <= 16:
+            print(error_messages[1][language])
+            raise ValidationError(error_messages[1][language])
+    except KeyError:
+        print(f'Не знаю такого языка!!')
+
