c7e1af3b6ca6eaae2239b35a50dfd9b35d0ce96c
romz987pc <eesistance@gmail.com>
2025-05-05 23:18:43 +0300

    feat: add caching and never_cache;

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path  
-from dogs.apps import DogsConfig 
+from dogs.apps import DogsConfig
+# Redis
+from django.views.decorators.cache import (
+    cache_page,
+    never_cache
+)
 from dogs.views import (
     DogBreedListView,
     IndexView,
@@ -10,9 +15,6 @@ from dogs.views import (
     DogDetailView,
     DogListView
 )
-# Redis  
-from django.views.decorators.cache import cache_page
-
 
 app_name = DogsConfig.name  
 
@@ -20,13 +22,13 @@ app_name = DogsConfig.name
 urlpatterns = [
     # breads
     path('', cache_page(60)(IndexView.as_view()), name='index'),
-    path('breeds/', BreedListView.as_view(), name='breeds'),    
+    path('breeds/', cache_page(60)(BreedListView.as_view()), name='breeds'),    
     
     # dogs
     path('breeds/<int:pk>/dogs', DogBreedListView.as_view(), name='breed_dogs'),
     path('dogs/', DogListView.as_view(), name='dogs_list'),
-    path('dogs/create', cache_page(60)(DogCreateView.as_view()), name='dog_create'),
+    path('dogs/create', DogCreateView.as_view(), name='dog_create'),
     path('dogs/detail/<int:pk>/', DogDetailView.as_view(), name='dog_detail'),
-    path('dogs/update/<int:pk>/', DogUpdateView.as_view(), name='dog_update'),
+    path('dogs/update/<int:pk>/', never_cache(DogUpdateView.as_view()), name='dog_update'),
     path('dogs/delete/<int:pk>/', DogDeleteView.as_view(), name='dog_delete')
 ]
