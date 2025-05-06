f9ba7ab9d2eb1ec2ab2dd81a9542537349cbc2f2
romz987pc <eesistance@gmail.com>
2025-05-06 23:46:39 +0300

    feat: create postgres database method;

--- users/management/commands/ccdb.py
+++ users/management/commands/ccdb.py
@@ -1,30 +1,47 @@
 from django.core.management import BaseCommand 
 
-import pyodbc 
+import psycopg2
+from psycopg2 import sql
 
-from config.settings import USER, PASSWORD, HOST, DRIVER, PAD_DATABASE, DATABASE 
+from django.conf import settings
+#from config.settings import USER, PASSWORD, HOST, DRIVER, PAD_DATABASE, DATABASE 
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        ConnectionString = f"""
-            DRIVER={DRIVER};
-            SERVER={SERVER};
-            DATABASE={PAD_DATABASE};
-            UID={USER};
-            PWD={PASSWORD};
-        """
+        PAD_DATABASE='postgres'
+        USER=settings.DATABASES['default']['USER']
+        PASSWORD=settings.DATABASES['default']['PASSWORD']
+        HOST=settings.DATABASES['default']['HOST']
+        PORT=settings.DATABASES['default']['PORT']
+        DATABASE=settings.DATABASES['default']['NAME']
         try:
-            conn = pyodbc.connect(ConnectionString)
-        except pyodbc.ProgrammingError as ex:
-            print(ex)
+            conn = psycopg2.connect(
+                dbname=PAD_DATABASE,
+                user=USER,
+                password=PASSWORD,
+                host=HOST,
+                port=PORT     
+            )
+            conn.autocommit = True
+            cursor = conn.cursor()
+
+            # Создаём БД, если не существует
+            cursor.execute(
+                sql.SQL("CREATE DATABASE {}").format(
+                    sql.Identifier(DATABASE)
+                )
+            )
+
+        except psycopg2.errors.DuplicateDatabase:
+            print(f'База данных {DATABASE} уже существует')
+        except Exception as e:
+            print(f'Ошибка при создании БД: {e}')
         else:
-            conn.autocommit = True 
-            try:
-                conn.execute(fr'CREATE DATABASE {DATABASE};')
-            except pyodbc.ProgrammingError as ex:
-                print(ex)
-            else:
-                print(f'База данных {DATABASE} успешно создана')
+            print(f'База данных {DATABASE} успешно создана')
+        finally:
+            if 'conn' in locals():
+                conn.close()
+
 
