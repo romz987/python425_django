42f9d1bfa3891b1b412a7e28b54fc415a5d5bdac
romz987pc <eesistance@gmail.com>
2025-05-06 22:56:53 +0300

    feat: 1.add UserRoles;  2.add role to User model;  3.migrations;

--- users/models.py
+++ users/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser 
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    ADMIN = 'admin', _('admin') 
+    MODERATOR = 'moderator', _('moderator')
+    USER = 'user', _('user')
 
 
 class User(AbstractUser):
 
     username = None 
     email = models.EmailField(unique=True, verbose_name='email')
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.USER)
     first_name = models.CharField(max_length=150, verbose_name='Имя', default='Anonymous')
     last_name = models.CharField(max_length=150, verbose_name='Фамилия', default='Anonymous')
     phone = models.CharField(max_length=35, unique=True, verbose_name='Номер телефона', **NULLABLE)
