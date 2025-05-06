3d138480a70134e2a5c7ed1f264cc31c029b9020
romz987pc <eesistance@gmail.com>
2025-05-06 23:55:36 +0300

    fix: 1.change list_display elements order;  2.add is_active;

--- users/admin.py
+++ users/admin.py
@@ -4,5 +4,5 @@ from users.models import User
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'email', 'pk')
+    list_display = ('pk', 'email', 'last_name', 'first_name', 'role', 'is_active')
     list_filter = ('last_name', )
