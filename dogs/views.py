375f151a68aff3e13656b2b14210cd1fb6990022
romz987pc <eesistance@gmail.com>
2025-05-07 21:16:26 +0300

    feat: 1.add get_queryset to DogListView;  2.add DogDeactivatedListView;

--- dogs/views.py
+++ dogs/views.py
@@ -8,6 +8,7 @@ from dogs.forms import DogForm, DogParentForm
 from django.http import HttpResponseRedirect 
 from django.urls import reverse, reverse_lazy  
 from django.forms import inlineformset_factory
+from users.models import UserRoles
 
 # Только авторизованные пользователи 
 from django.contrib.auth.decorators import login_required
@@ -66,6 +67,30 @@ class DogListView(ListView):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Все наши собаки'
         return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset() 
+        queryset = queryset.filter(is_active=True)
+        return queryset
+
+
+class DogDeactivatedListView(LoginRequiredMixin, ListView):
+    model = Dog 
+    template_name = 'dogs/dogs.html'
+    context_object_name = 'objects_list'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Все наши собаки'
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset() 
+        if self.request.user.role in [UserRoles.ADMIN, UserRoles.MODERATOR]:
+            queryset = queryset.filter(is_active=False)
+        if self.request.user.role == UserRoles.USER:
+            queryset = queryset.filter(is_active=False, owner=self.request.user)
+        return queryset
 
 
 class DogCreateView(LoginRequiredMixin, CreateView):
@@ -176,106 +201,5 @@ class DogDeleteView(LoginRequiredMixin, DeleteView):
         if self.object.owner != self.request.user and not self.request.user.is_staff:
             raise Http404
         return self.object
-
-
-# @login_required(login_url='users:user_login')
-# def dog_delete_view(request, pk):
-#     """ Удалить собаку """
-#     dog_object = get_object_or_404(Dog, pk=pk)
-#     if request.method == 'POST':
-#         dog_object.delete()
-#         return HttpResponseRedirect(reverse('dogs:dogs_list'))
-#     context = {
-#         'object': dog_object,
-#         'title': 'Удалить собаку'
-#     }
-#     return render(request, 'dogs/delete.html', context=context)
-
-
-# CRUD 
-# def dog_create_view(request):
-#     """ Для создания нового объекта и помещения его в базу """
-#     if request.method == 'POST':
-#         form = DogForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('dogs:dogs_list'))
-#     context = {
-#         'title': 'Добавить собаку',
-#         'form': DogForm
-#     }
-#     return render(request, 'dogs/create.html', context=context)
-
-
-# @login_required(login_url='users:user_login')
-# def dog_create_view(request):
-#     """ Создать собаку """
-#     if request.method == 'POST':
-#         form = DogForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             dog_object = form.save()
-#             dog_object.owner = request.user 
-#             dog_object.save()
-#             #form.save()
-#             return HttpResponseRedirect(reverse('dogs:dogs_list'))
-#         else:
-#             print(form.errors)  # Добавьте эту строку для отладки
-#     else:
-#         form = DogForm()
-#
-#     context = {
-#         'title': 'Добавить собаку',
-#         'form': form
-#     }
-#     return render(request, 'dogs/create_update.html', context=context)
-
-
-# @login_required(login_url='users:user_login')
-# def dog_detail_view(request, pk):
-#     """ Вернуть детальную информацию о собаке """
-#     dog_object = Dog.objects.get(pk=pk)
-#     context = {
-#         'object': dog_object,
-#         'title': dog_object.name + ' - ' + dog_object.breed.name
-#     }
-#     return render(request, 'dogs/detail.html', context=context)
-
-
-# def dog_list_view(request):
-#     context = {
-#         'objects_list': Dog.objects.all(),
-#         'title': f'Все наши собаки',
-#     }   
-#     return render(request, 'dogs/dogs.html', context)
-
-
-# # отображение всех объектов независимо от модели 
-# def breeds_list_view(request):
-#     context = {
-#         'objects_list': Breed.objects.all(),
-#         'title': 'Питомник - Все наши породы'
-#     }
-#     return render(request, 'dogs/breeds.html', context=context)
-
-
-#
-# # Create your views here.
-# def index_view(request):
-#     context = {        
-#         'objects_list': Breed.objects.all()[:3],
-#         'title': 'Питомник - Главная страница'
-#     }
-#     return render(request, 'dogs/index.html', context=context)
-
-
-# def breed_dogs_list_view(request, pk: int):
-#     breed_object = Breed.objects.get(pk=pk)
-#     context = {
-#         'objects_list': Dog.objects.filter(breed_id=pk),
-#         'title': f'Собаки породы - {breed_object}',
-#         'breed_pk': breed_object.pk
-#     }
-#     return render(request, 'dogs/dogs.html', context=context)
-
 
 
