a008fc42a63b25f1ac3bbe0da13da7b54d68ca23
romz987pc <eesistance@gmail.com>
2025-05-15 23:14:52 +0300

    feat: 1.add CRUD for Reviews;  2.add urls for CRUD;  3.create empty html templates;  4.add review_toggle_activity;

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,12 +2,25 @@ from django.urls import path
 
 from reviews.apps import ReviewsConfig 
 
-from reviews.views import ReviewListView, ReviewDeactivatedListView
+from reviews.views import (
+    ReviewListView, 
+    ReviewDeactivatedListView, 
+    ReviewCreateView, 
+    ReviewDetailView,
+    ReviewUpdateView, 
+    ReviewDeleteView,
+    review_toggle_activity
+) 
 
 app_name = ReviewsConfig.name
 
 urlpatterns = [
     path('', ReviewListView.as_view(), name='reviews_list'),
     path('deactivated/', ReviewDeactivatedListView.as_view(), name='reviews_deactivated_list'),
+    path('create/', ReviewCreateView.as_view(), name='review_create'),
+    path('detail/<slug:slug>/', ReviewDetailView.as_view(), name='review_detail'),
+    path('update/<slug:slug>/', ReviewUpdateView.as_view(), name='review_update'),
+    path('delete/<slug:slug>/', ReviewDeleteView.as_view(), name='review_delete'),
+    path('toggle/<slug:slug>/', review_toggle_activity, name='review_toggle'),
 ]
 
--- reviews/views.py
+++ reviews/views.py
@@ -8,30 +8,80 @@ from django.views.generic import (
     DeleteView,
 )
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.core.exceptions import PermissionDenied 
+from django.core.exceptions import PermissionDenied
 
-from reviews.models import Review 
-from users.models import User 
+from reviews.models import Review
+from users.models import User, UserRoles
 from reviews.forms import ReviewAdminForm
 
 
 class ReviewListView(ListView):
-    model = Review 
-    extra_context = {
-        'title': 'Все отзывы'
-    }
-    template_name = 'reviews/reviews.html'
+    model = Review
+    extra_context = {"title": "Все отзывы"}
+    template_name = "reviews/reviews.html"
 
     def get_queryset(self):
         return super().get_queryset().filter(sign_of_review=True)
 
 
 class ReviewDeactivatedListView(ListView):
-    model = Review 
-    extra_context = {
-        'title': 'Неактивные отзывы'
-    }
-    template_name = 'reviews/reviews.html'
+    model = Review
+    extra_context = {"title": "Неактивные отзывы"}
+    template_name = "reviews/reviews.html"
 
     def get_queryset(self):
         return super().get_queryset().filter(sign_of_review=False)
+
+
+class ReviewCreateView(LoginRequiredMixin, CreateView):
+    model = Review
+    form_class = ReviewAdminForm
+    template_name = "reviews/create_update.html"
+    extra_context = {"title": "Добавить отзыв"}
+
+
+class ReviewDetailView(LoginRequiredMixin, DetailView):
+    model = Review
+    template_name = "reviews/detail.html"
+    extra_context = {"title": "Просмотр отзыва"}
+
+
+class ReviewUpdateView(LoginRequiredMixin, UpdateView):
+    model = Review
+    form_class = ReviewAdminForm
+    template_name = "reviews/create_update.html"
+    extra_context = {"title": "Изменить отзыв"}
+
+    def get_object(self, queryset=None):
+        object_ = super().get_object(queryset=queryset)
+        if object_.author != self.request.user and self.request.user.role not in [
+            UserRoles.ADMIN,
+            UserRoles.MODERATOR,
+        ]:
+            raise PermissionDenied()
+        return object_
+
+    def get_succuss_url(self):
+        return reverse("review:review_detail", args=[self.kwargs.get("slug")])
+
+
+class ReviewDeleteView(PermissionRequiredMixin, DeleteView):
+    model = Review 
+    template_name = 'reviews/delete.html'
+    permission_required = 'reviews.delete_review'
+
+    def get_success_url(self):
+        return reverse('reviews:reviews_list')
+
+
+def review_toggle_activity(request, slug):
+    # 3:28
+    review_item = get_object_or_404(Review, slug=slug)
+    if review_item.sign_of_review:
+        review_item.sign_of_review = False 
+        review_item.save() 
+        return redirect(reverse('reviews:reviews_deactivated_list'))
+    else:
+        review_item.sign_of_review = True  
+        review_item.save()
+        return redirect(reverse('reviews:reviews_list'))
